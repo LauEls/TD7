440ba4122b3ed728118217e5246ca1fe0cf61391
LauEls <44226646+LauEls@users.noreply.github.com>
2024-06-16 17:15:16 +0200

    new runs

--- main_robosuite.py
+++ main_robosuite.py
@@ -108,8 +108,8 @@ def maybe_evaluate_and_print(RL_agent, eval_env, evals, t, start_time, args, d4r
 if __name__ == "__main__":
 	experimental_runs = 3
 	for i in range(experimental_runs):
-		# load_dir = "runs/lift/panda/osc_pose/offline/v2_medium_expert_with_random"
-		load_dir = "runs/lift/panda/osc_pose/online/v3_ada527_reward_shaping"
+		load_dir = "runs/lift/panda/osc_pose/offline/v3_expert_with_random"
+		# load_dir = "runs/lift/panda/osc_pose/online/v4_ada527_no_reward_shaping"
 		# load_dir = "runs/stack/panda/osc_pose/online/v1"
 
 		kwargs_fpath = os.path.join(load_dir, "variant.json")
