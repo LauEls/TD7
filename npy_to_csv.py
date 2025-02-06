8674cc122ff7e2ad071cc90be2666572a1976bcf
LauEls <laurenz.elstner@gmail.com>
2025-02-06 10:36:33 +0100

    new run prep

--- npy_to_csv.py
+++ npy_to_csv.py
@@ -2,15 +2,18 @@ import numpy as np
 import os
 import csv
 
-file_base_path = "runs/lift/panda/osc_pose/online/v8_reduced_ep_len_500/run_1"
-# file_base_path = "runs/lift/panda/osc_pose/offline/v5_medium_expert_2/run_0"
-npy_file_name = "results.npy"
-csv_file_name = "results.csv"
+for i in range(1):
 
-npy_data = np.load(os.path.join(file_base_path,npy_file_name), allow_pickle=True)
-#data_mean = npy_data.mean(axis=1)
+    # file_base_path = "runs/door_mirror/gh360/osc_pose/v3_ep_length_50/run_"+str(i)
+    file_base_path = "runs/door_mirror/gh360/joint_velocity/offline/v1_first_offline_test/run_"+str(i)
+    # file_base_path = "runs/lift/panda/osc_pose/offline/v5_medium_expert_2/run_0"
+    npy_file_name = "results.npy"
+    csv_file_name = "results.csv"
 
-with open(os.path.join(file_base_path,csv_file_name), mode='w') as csv_file:
-    csvwriter = csv.writer(csv_file)
-    csvwriter.writerows(npy_data)
+    npy_data = np.load(os.path.join(file_base_path,npy_file_name), allow_pickle=True)
+    #data_mean = npy_data.mean(axis=1)
+
+    with open(os.path.join(file_base_path,csv_file_name), mode='w') as csv_file:
+        csvwriter = csv.writer(csv_file)
+        csvwriter.writerows(npy_data)
 
