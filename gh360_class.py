039154875de7f70ad280cb21e9816ff349d95dc2
LauEls <laurenz.elstner@gmail.com>
2025-04-03 16:14:21 +0200

    feat: gh360_gui

---
+++ gh360_class.py
@@ -0,0 +1,256 @@
+import TD7
+import os
+import json
+import gym
+import gh360_gym
+import numpy as np
+import argparse
+import torch
+import time
+
+class RL_GH360:
+    def __init__(self, env_name, exp_runs, config_file_path):
+        load_dir = config_file_path
+        self.exp_run = 0
+        self.stop = False
+        self.pause = False
+
+        kwargs_fpath = os.path.join(load_dir, "variant.json")
+        try:
+            with open(kwargs_fpath) as f:
+                variant = json.load(f)
+        except FileNotFoundError:
+            print("Error opening default controller filepath at: {}. "
+                "Please check filepath and try again.".format(kwargs_fpath))
+            
+        state_file = os.path.join(load_dir, "state.json")
+        try:
+            with open(state_file) as f:
+                state = json.load(f)
+        except FileNotFoundError:
+            print("Error opening default controller filepath at: {}. "
+                "Please check filepath and try again.".format(state_file))
+            
+        env_config = variant["environment_kwargs"]
+        env_name = variant["environment_kwargs"].pop("env_name")
+        seed = variant["seed"]
+        self.offline = variant["offline"]
+
+        self.env = gym.make('gh360_gym/'+env_name, **env_config)
+        self.eval_env = self.env
+
+        # parser = argparse.ArgumentParser()
+        # RL
+        # parser.add_argument('--use_checkpoints', default=True)
+        # # Evaluation
+        # parser.add_argument("--timesteps_before_training", default=25e3, type=int)
+        # parser.add_argument("--eval_freq", default=5e3, type=int)
+        # parser.add_argument("--eval_eps", default=5, type=int)
+        # parser.add_argument("--max_timesteps", default=5e6, type=int)
+        # parser.add_argument("--ep_length", default=500, type=int)
+        # File
+        # parser.add_argument('--file_name', default=None)
+        # parser.add_argument('--d4rl_path', default="./d4rl_datasets", type=str)
+        # parser.add_argument('--result_path', default="./results", type=str)
+        # parser.add_argument('--rollout', default=False, type=bool)
+        # args = parser.parse_args()
+
+        self.use_checkpoints = True
+        self.ep_length = variant["episode_length"]
+        self.timesteps_before_training = self.ep_length*50
+        self.eval_freq = self.ep_length*10
+        self.max_timesteps = self.ep_length*10000
+        self.init_buffer_paths = variant["init_buffer_paths"]
+        self.eval_eps = 1
+
+        # args.ep_length = variant["episode_length"]
+        # args.timesteps_before_training = args.ep_length*50
+        # args.eval_freq = args.ep_length*10
+        # args.max_timesteps = args.ep_length*10000
+        # # variant["hyperparameters"]["buffer_size"] = args.ep_length*2000
+        # # variant["hyperparameters"]["batch_size"] = int(args.ep_length/2)
+        # args.init_buffer_paths = variant["init_buffer_paths"]
+        
+        # args.eval_eps = 1
+
+
+        if self.offline:
+            expert_paths = np.load(os.path.join("demonstrations/",variant["demo_file_name"]), allow_pickle=True)
+            print("Expert paths shape: ", expert_paths.shape)
+            random_paths_file = os.path.join("demonstrations/",variant["demo_file_name"])
+            random_paths_file = random_paths_file[0:-4] + "_random_paths" + random_paths_file[-4:]
+            print(random_paths_file)
+            random_paths = np.load(random_paths_file, allow_pickle=True)
+            print("Random paths shape: ", random_paths.shape)
+            paths = np.concatenate((expert_paths, random_paths))
+            self.use_checkpoints = False
+            self.eval_during_training = variant["eval_during_training"]
+
+        # if args.file_name is None:
+        #     args.file_name = f"TD7_{env_name}_{seed}"
+
+
+        self.result_path = os.path.join(load_dir, "run_"+str(self.exp_run))
+
+        if not os.path.exists(self.result_path):
+            os.makedirs(self.result_path)
+        
+
+        
+
+        print("---------------------------------------")
+        print(f"Algorithm: TD7, Env: {env_name}, Seed: {seed}")
+        print("---------------------------------------")
+
+        self.env.seed(seed)
+        self.env.action_space.seed(seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+        if self.offline:
+            # print("Expert paths: ", expert_paths[0]['actions'].shape)
+            # print("Random paths: ", random_paths[0]['actions'].shape)
+            # print("Paths: ", paths[0]['observations'].shape)
+            # print("Paths: ", paths[30]['observations'].shape)
+            state_dim = paths[0]['observations'].shape[1]
+            action_dim = paths[0]['actions'].shape[1]
+            max_action = 0.0
+            for path in paths:
+                new_max_action = np.max(np.abs(path['actions']))
+                if new_max_action > max_action: max_action = new_max_action
+            # print("Max action: ", max_action)
+        else:
+            state_dim = self.env.observation_space.shape[0]
+            action_dim = self.env.action_space.shape[0] 
+            max_action = float(self.env.action_space.high[0])
+
+        print("State dim: ", state_dim)
+        print("Action dim: ", action_dim)
+        print("Max action: ", max_action)
+        hp = TD7.Hyperparameters(**variant["hyperparameters"])
+        hp.dir_path = self.result_path
+        
+        self.RL_agent = TD7.Agent(state_dim, action_dim, max_action, offline=self.offline, hp=hp)
+
+    def start_learning(self):
+        if self.offline:
+            self.train_offline()
+        else:
+            self.train_online()
+
+
+    def train_online(self):
+        t = 0
+        if self.RL_agent.continue_learning:
+            buffer_paths = np.load(self.result_path+"/buffer_paths.npy", allow_pickle=True)
+            self.RL_agent.replay_buffer.load_paths(buffer_paths)
+        elif self.init_buffer_paths:
+            buffer_paths = np.load(self.result_path+"/init_buffer_paths.npy", allow_pickle=True)
+            self.RL_agent.replay_buffer.load_paths(buffer_paths)
+            print(f"Loaded initial buffer paths of length: {len(buffer_paths[0]['observations'])}")
+            print(f"Buffer size: {self.RL_agent.replay_buffer.size}")
+            t = self.RL_agent.replay_buffer.size-1
+            
+
+        self.evals = []
+        start_time = time.time()
+        allow_train = False
+
+        state, ep_finished = self.env.reset(), False
+        ep_total_reward, ep_timesteps, ep_num = 0, 0, 1
+
+        # for i in range(int(args.max_timesteps+1)):
+        while t < int(self.max_timesteps+1):
+            self.maybe_evaluate_and_print(t, start_time)
+            
+            if allow_train:
+                action = self.RL_agent.select_action(np.array(state))
+            else:
+                action = self.env.action_space.sample()
+
+            next_state, reward, ep_finished, _ = self.env.step(action) 
+            
+            
+            ep_total_reward += reward
+            ep_timesteps += 1
+
+            if ep_timesteps >= self.ep_length: 
+                ep_finished = 1
+                self.env.step(np.zeros(self.RL_agent.action_dim))
+            # done = float(ep_finished) if ep_timesteps < 500 else 0
+            done = ep_finished
+            
+            self.RL_agent.replay_buffer.add(state, action, next_state, reward, done)
+
+            state = next_state
+
+            if allow_train and not self.use_checkpoints:
+                self.RL_agent.train()
+
+            if ep_finished: 
+                print(f"Reward: {ep_total_reward}")
+                print(f"Total T: {t+1} Episode Num: {ep_num} Episode T: {ep_timesteps} Reward: {ep_total_reward:.3f}")
+
+                if allow_train and self.use_checkpoints:
+                    self.RL_agent.maybe_train_and_checkpoint(ep_timesteps, ep_total_reward)
+
+                if t >= self.timesteps_before_training:
+                    allow_train = True
+                    if t >= self.timesteps_before_training and t <= (self.timesteps_before_training+ep_timesteps):
+                        print("Saving initial buffer paths")
+                        self.RL_agent.replay_buffer.save_paths(self.result_path+"/init_buffer_paths.npy")
+
+                state, done = self.env.reset(), False
+                ep_total_reward, ep_timesteps = 0, 0
+                ep_num += 1 
+
+                while self.pause:
+                    time.sleep(0.1)
+                
+                if self.stop:
+                    self.save_training_state()
+                    return
+
+            t += 1
+
+        # Save final model
+        self.RL_agent.save_model(self.result_path)
+        self.RL_agent.replay_buffer.save_paths(os.path.join(self.result_path,"buffer_paths.npy"))
+        self.RL_agent.replay_buffer.save_priority(os.path.join(self.result_path, "priority.npy"))
+        self.RL_agent.save_class_variables(self.result_path)
+    
+    def train_offline(self):
+        pass
+
+    def maybe_evaluate_and_print(self, t, start_time):
+        if t % self.eval_freq == 0 and t > 0:
+            print("---------------------------------------")
+            print(f"Evaluation at {t} time steps")
+            print(f"Total time passed: {round((time.time()-start_time)/60.,2)} min(s)")
+
+            total_reward = np.zeros(self.eval_eps)
+            for ep in range(self.eval_eps):
+                state, done = self.eval_env.reset(), False
+                cntr = 0
+                while not done and cntr < self.ep_length:
+                    action = self.RL_agent.select_action(np.array(state), self.use_checkpoints, use_exploration=False)
+                    state, reward, done, _ = self.eval_env.step(action)
+                    total_reward[ep] += reward
+                    cntr += 1
+                            
+            print(f"Average total reward over {self.eval_eps} episodes: {total_reward.mean():.3f}")
+
+            print("---------------------------------------")
+
+            self.evals.append(total_reward)
+            # np.save(f"./results/{args.file_name}", evals)
+            np.save(os.path.join(self.result_path,"results.npy"), self.evals)
+
+            self.eval_env.reset()
+
+    def save_training_state(self):
+        self.RL_agent.save_model(self.result_path)
+        self.RL_agent.replay_buffer.save_paths(os.path.join(self.result_path,"buffer_paths.npy"))
+        self.RL_agent.replay_buffer.save_priority(os.path.join(self.result_path, "priority.npy"))
+        self.RL_agent.save_class_variables(self.result_path)
+        pass
---
+++ gui_gh360.py
@@ -0,0 +1,104 @@
+import os
+import re
+import sys
+import TKinterModernThemes as TKMT
+import tkinter as tk
+from gh360_class import RL_GH360
+
+
+class GH360LearningGUI(TKMT.ThemedTKinterFrame):
+    def __init__(self):
+        super().__init__("Learning Experiments", "sun-valley", "dark")
+        
+        self.Text("Environment: ", row=0, col=0)
+        self.env_option_menu_list = ["Door"]
+        self.env_name = tk.StringVar(value=self.env_option_menu_list[0])
+        self.OptionMenu(self.env_option_menu_list, self.env_name, row=0, col=1)
+        self.env_name.trace_add("write", self.options_changed)
+
+        self.Text("Controller: ", row=1, col=0)
+        self.controller_option_menu_list = ["EEF Velocity", "Motor Velocity"]
+        self.controller_name = tk.StringVar(value=self.controller_option_menu_list[0])
+        self.OptionMenu(self.controller_option_menu_list, self.controller_name, row=1, col=1)
+        self.controller_name.trace_add("write", self.options_changed)
+        
+        self.Text("Learning Mode: ", row=2, col=0)
+        self.learning_mode_option_menu_list = ["online", "offline"]
+        self.learning_mode_name = tk.StringVar(value=self.learning_mode_option_menu_list[0])
+        self.OptionMenu(self.learning_mode_option_menu_list, self.learning_mode_name, row=2, col=1)
+        self.learning_mode_name.trace_add("write", self.options_changed)
+
+        self.Text("Config Folder: ", row=3, col=0)
+        self.config_folder_option_menu_list = self.parse_config_folders()
+        self.config_folder_name = tk.StringVar(value=self.config_folder_option_menu_list[0])
+        self.opmen_config_folder = self.OptionMenu(self.config_folder_option_menu_list, self.config_folder_name, row=3, col=1)
+
+        self.Text("Experimental Runs: ", row=4, col=0)
+        self.experimental_runs = tk.IntVar(value=1)
+        exp_runs_obj = self.NumericalSpinbox(1,10,1,self.experimental_runs, row=4, col=1)
+        exp_runs_obj.state(["readonly"])
+        
+
+        self.btn_load_config = self.AccentButton("Load Config", row=5, col=0, colspan=2, command=self.load_config)
+        self.btn_start = self.AccentButton("Start", row=6, col=0, command=self.start_experiment)
+        self.btn_start.state(["disabled"])
+        self.btn_stop = self.AccentButton("Stop", row=6, col=1, command=self.stop_experiment)
+        self.btn_stop.state(["disabled"])
+
+        self.run()
+
+    def options_changed(self, *args):
+        self.config_folder_option_menu_list = self.parse_config_folders()
+        self.config_folder_name = tk.StringVar(value=self.config_folder_option_menu_list[0])
+        self.opmen_config_folder.set_menu(self.config_folder_name.get(), *self.config_folder_option_menu_list)
+
+    def parse_config_folders(self):
+        config_folders = []
+
+        config_path = "runs/"
+        #Environment
+        if self.env_name.get() == "Door":
+            config_path += "door/"
+        
+        config_path += "real_gh360/"
+
+        if self.controller_name.get() == "EEF Velocity":
+            config_path += "eef_vel/"
+        elif self.controller_name.get() == "Motor Velocity":
+            config_path += "motor_vel/"
+
+        config_path += self.learning_mode_name.get() + "/"
+
+        # dir_lvl = 0
+        for root, dirs, files in os.walk(config_path):
+            for dir in dirs:
+                config_folders.append(dir)
+                print("Found config folder:", dir)
+            break
+
+        self.config_path = config_path
+        return config_folders
+    
+    def load_config(self):
+        # Placeholder for loading config
+        print("Loading config...")
+        self.btn_load_config.state(["disabled"])
+
+        self.learning_class = RL_GH360(self.env_name.get(), self.experimental_runs, self.config_path + self.config_folder_name.get())
+
+        self.btn_start.state(["!disabled"])
+
+    def start_experiment(self):
+        # Placeholder for starting experiment
+        print("Starting experiment...")
+        self.btn_start.state(["disabled"])
+        self.learning_class.start_learning()
+        self.btn_stop.state(["!disabled"])
+
+    def stop_experiment(self):
+        # Placeholder for stopping experiment
+        print("Stopping experiment...")
+        self.btn_stop.state(["disabled"])
+        self.btn_start.state(["!disabled"])
+
+GH360LearningGUI()
